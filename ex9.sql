ac5f08b635184e2db0a39c9c3843f9efc95bf8c5
Mateus Cruz <mshcruz@gmail.com>
2016-05-08 08:43:25 +0900

    Adding ex9

---
+++ ex9.sql
@@ -0,0 +1,20 @@
+UPDATE person SET first_name = "Hilarious Guy"
+	WHERE first_name = "Zed";
+
+UPDATE pet SET name = "Fancy Pants"
+	WHERE id=0;
+
+SELECT * FROM person;
+SELECT * FROM pet;
+
+UPDATE person SET first_name = "Zed"
+	WHERE id = 0;
+
+UPDATE pet SET dead = 1
+	WHERE id = 1;
+
+UPDATE pet SET name = "DECEASED"
+	WHERE dead = 1;
+
+SELECT * FROM person;
+SELECT * FROM pet;
