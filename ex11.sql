9ebc534ffaf8c5c549c1c71ca2d0ea1909b4c5c6
Mateus Cruz <mshcruz@gmail.com>
2016-05-09 18:51:07 +0900

    Adding ex11

---
+++ ex11.sql
@@ -0,0 +1,22 @@
+/* This should fail because 0 is already taken. */
+INSERT INTO person (id, first_name, last_name, age)
+	VALUES (0, 'Frank', 'Smith', 100);
+
+/* We can force it by doing an INSERT OR REPLACE. */
+INSERT OR REPLACE INTO person (id, first_name, last_name, age)
+	VALUES (0, 'Frank', 'Smith', 100);
+
+SELECT * FROM person;
+
+/* And shorthand for that is just REPLACE. */
+REPLACE INTO person (id, first_name, last_name, age)
+	VALUES (0, 'Zed', 'Shaw', 37);
+
+/* Now you can see I'm back. */
+SELECT * FROM person;
+
+/* Replace unicorn with a parrot*/
+INSERT OR REPLACE INTO pet (id, name, breed, age, dead)
+	VALUES (0, "Zed's Pet", "Parrot", 10, 0);
+
+SELECT * FROM pet;
